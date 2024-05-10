85d9eec2b934a1a031f5bd42822d0ef9d5a16fcd
dongceha <30817210+dongceha@users.noreply.github.com>
2024-05-10 22:53:03 +0800

    更新

--- assets/contentScript.js
+++ assets/contentScript.js
@@ -1,1 +1,26 @@
-console.log('总算被我找到了1！！！！！！')
+
+// new PerformanceObserver((entryList) => {
+//   for (const entry of entryList.getEntries()) {
+//     console.log('LCP candidate:', entry.startTime, entry);
+//   }
+// }).observe({
+//   type: 'largest-contentful-paint',
+//   buffered: true
+// });
+
+const injectScript = (file, node) => {
+  const th = document.querySelector(node);
+  const s = document.createElement('script');
+  s.setAttribute('type', 'text/javascript');
+  s.setAttribute('async', 'async');
+  s.setAttribute('src', file);
+  th.appendChild(s);
+  return s
+};
+(function() {
+  injectScript(chrome.runtime.getURL('/js/proxy_encrypt.js'), 'body');
+  const sc = injectScript(chrome.runtime.getURL('/js/web-vitals.iife.js'), 'body');
+  sc.onload = () => {
+    injectScript(chrome.runtime.getURL('/js/do_webVitals.js'), 'body');
+  }
+})();
---
+++ js/do_webVitals.js
@@ -0,0 +1,7 @@
+//     // When loading `web-vitals` using a classic script, all the public
+//     // methods can be found on the `webVitals` global namespace.
+console.log('do_webVitals====', webVitals)
+
+webVitals.onCLS(console.log);
+webVitals.onFID(console.log);
+webVitals.onLCP(console.log);
